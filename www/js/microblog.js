4e620c55342e9e40d90f995696857370a6b3028d
electronic Max <electronic@gmail.com>
2016-02-26 14:20:36 +0000

    now working

--- www/js/microblog.js
+++ www/js/microblog.js
@@ -1,24 +1,24 @@
 /* global console,  angular, $, window */
 
-angular.module('dtu', [])
+angular.module('dtou', [])
     .controller('microblog', function($scope) {
 
         window.postDoc = (collection, id, doc, dtou) => {
-            return $.ajax({url:`/get/${collection}/${id}`,
-                contentType:'application/json',
-                method:'POST',
-                data:JSON.stringify(doc),
-                headers: dtou !== undefined ? { dtou:JSON.stingify(dtou) } : undefined
-            }).then((x) => {console.log(x); return x;});
+            return $.ajax({url:`/api/${collection}/${id}`,
+                    contentType:'application/json',
+                    method:'post',
+                    data:JSON.stringify(doc),
+                    headers: dtou !== undefined ? { dtou:JSON.stingify(dtou) } : undefined
+            }); // .then((x) => {console.log(x); return x;});
         };
 
         window.getDoc = (collection, id, dtou_asserts) => {
-            return $.ajax({url:`/get-dtou/${collection}/${id}`,
+            return $.ajax({url:`/api/${collection}/${id}`,
                 contentType:'application/json',
                 method:'GET',
                 headers: dtou_asserts !== undefined ? {
                     dtou:JSON.stingify(dtou_asserts)
                 } : undefined
-            }).then((x) => {console.log(x); return x;});
+            }); // .then((x) => {console.log(x); return x;});
         };
     });
