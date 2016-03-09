35a439c27c9585ba5d60f8a6963abbeeb8b1e5f5
electronic Max <electronic@gmail.com>
2016-03-09 15:04:53 +0000

    new auth modules

---
+++ smvm-auth.js
@@ -0,0 +1,55 @@
+/* jshint strict:false */
+/* global console, require, process */
+
+
+var conn_by_token = {},
+	crypto = require('crypto');
+
+var makeToken = () => { 
+	return new Promise((res,rej) => { 
+		crypto.randomBytes(48, (ex, buf) => {  res(buf.toString('hex')); });
+	});
+};
+
+app.post('/api/auth', (req,res) => {
+	// automatically auth
+	var username = req.body.username.trim(),
+		password = req.body.password;
+
+	console.info(req.body, 'username ', username, 'passwd ', password);	
+
+	db.collection('users').findById(username).then(function(userdoc) { 
+		console.log('userdoc ', userdoc);
+		if (userdoc.passhash == md5(password)) { 
+			makeToken().then((token) => { 
+				console.info('password matches for user ', username, ' setting token ', token);			
+				res.cookie('authtoken', token);
+				conn_by_token[token] = { user: username, db_connections: {} };
+				return res.status(200).send();
+			});
+		} else {
+			res.status(403).send();
+		}
+	});
+});
+/* creates a new user document - overwriting previous ones. this
+   effectively negates all security */
+app.post('/api/newuser', (req,res) => {
+	// todo make this actually work.
+	var username = req.body.username.trim(),
+		password = req.body.password,
+		passhash = md5(password);
+
+	db.collection('users').save({_id:username, passhash:passhash}).then(function() { 
+		res.status(200).send();
+	});
+});
+// auth check
+app.get('/api/check', (req,res) => {
+	var token = req.cookies.authtoken;
+	console.info('auth cookie is ', req.cookies, token);
+	if (conn_by_token[token] !== undefined) {
+		return res.status(200).send();
+	}
+	return res.status(403).send();
+});
---
+++ smvm-crypto.js
@@ -0,0 +1,14 @@
+/* jshint strict:false */
+/* global console, require, process */
+
+var crypto = require('crypto');
+
+var prime_length = 60;
+var diffHell = crypto.createDiffieHellman(prime_length);
+
+diffHell.generateKeys('base64');
+console.log("Public Key : " ,diffHell.getPublicKey('base64'));
+console.log("Private Key : " ,diffHell.getPrivateKey('base64'));
+
+console.log("Public Key : " ,diffHell.getPublicKey('hex'));
+console.log("Private Key : " ,diffHell.getPrivateKey('hex'));
