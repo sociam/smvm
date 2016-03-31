2f298e45dd902364d27f51ab2f655ab87f302f0f
electronic Max <max@hip.cat>
2016-03-31 01:45:40 +0100

    starting to mockup a component

--- smvm-components.js
+++ smvm-components.js
@@ -8,3 +8,18 @@ module.exports = {
 		console.info('registering components functions >>>>>>>>>> ');
 	}
 };
+
+var closedVote = (instance, whitelist_verifier, candidates) => {
+	var votes = instance.getStateVariable('votes');
+	return (voter, ballot) => {
+		return whitelist_verifier(voter).then((authorised) => {
+			if (!authorised) { throw Error("Not authorised to vote in this election"); }
+			if (candidates.indexOf(ballot.choice) >= 0) {
+				votes[voter.id] = ballot.choice;
+				return 'ok';
+			}
+			throw Error("Invalid vote ", ballot.choice);
+		});
+	};
+};
+
