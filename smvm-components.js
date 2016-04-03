e9f17c25dd043c3aa866f598f5a48dbf2e042d07
electronic Max <electronic@gmail.com>
2016-04-03 01:24:04 +0100

    components

--- smvm-components.js
+++ smvm-components.js
@@ -4,35 +4,47 @@
 const uuid = require('node-uuid'),
 	COLLECTION = COLLECTION;
 
-var SMOp = (sm, options) => {
+var SMOp = (sm, iid) => {
 	var this_ = this;
 	this.sm = sm;
-	this.id = opid;
-	this.config = config;
-
-	// load this one
-
-	if (options.iid) {
-
-		this.sm.smvm.db.collection(COLLECTION).get({_id:options.iid}).then((idoc) => {
-			this_.config.this_.deserialise_config(idoc);
-		});
-
-
+	this.iid = iid;
 };
 
 SMOp.prototype = {
-	getIDoc:() => {
-		var idb = this.sm.smvm.db.collection(COLLECTION);
-		return idb.find({iid:this.config.iid});
-	},
+	getIDoc: () => this.sm.smvm.db.collection(COLLECTION).findById(this.iid),
 	getRequestUser: (req) => require('./smvm-net').getRequestUser(req),	
+	load:() => { 	
+		return this.getIDoc().then((idoc) => {
+			this_.deserialise_config(idoc);
+			return this_.bind();
+		});
+	},
+	makeCallable:() => {
+		// weird POST-GET to deal with persisting values then getitng them.
+		return (args) => { 
+			return this_.getIDoc().then((idoc) => { 
+				var urls = idoc.urls;
+				// first set the aprams
+				return Promise.any(idoc.urls.map((url) => $.ajax({url:url, method:'POST', contentType:'application/json', data:JSON.stringify(args) })));
+			}).then((x) => {
+				// then get the value
+				return Promise.any(idoc.urls.map((url) => $.ajax({url:url, method:'GET' })));
+			});
+		};
+	},
 	bind:() => {
 		var this_ = this,
-			iid = this.config.iid
+			iid = this.config.iid,
+			urlpath = '/instances/'+iid,
+			net = require('./smvm-net'),
 			idb = this.sm.smvm.db.collection(COLLECTION);
 
-			this_.smvm.app.post('/instances/'+iid, (req,res) => { 
+		return this_.getIDoc().then((idoc) => { 
+			// update locations first
+			idoc.urls = net.makeFullURL(urlpath);
+			return idoc.save();
+		}).then(() => { 
+			this_.smvm.app.post(, (req,res) => { 
 				this_.getIDoc().then((idoc) => { 
 					// post of req, update our document
 					var user = this_.getRequestUser(req),
@@ -46,51 +58,29 @@ SMOp.prototype = {
 			this.smvm.app.get('/instances/'+i.config.iid, (req,res) => {
 				// now get arguments and apply
 				// invert params by identity
-				var protid = i.protid,
-					protocol = require('./smvm-examples').getRegistry()[protid],
-					method = protocol && protocol.methods && i.method,
-					config = this_.deserialise_config(i.config),
-					fn = protocol.method || protocol.methods && method && protocol.methods[method](this_,config);
+				this_.getIDoc().then((i) => { 				
+					var protid = i.protid,
+						protocol = require('./smvm-registry').getRegistry()[protid],
+						config = this_.sm.deserialise_op_callable_config(i.config),
+						fn = protocol(this_, config);
 
-				if (!protid) { 	return res.status(400).send('no protocol id specified'); }
-				if (!protocol) { return res.status(400).send('no known protocol ' + protid); }
-				if (protocol.methods && !method) { 
-					return res.status(400).send('no method for protocol ' + protid);						
-				}		
-				/// TODO --- next step is to apply the method to the arguments	
+					if (!protid) { 	return res.status(400).send('no protocol id specified'); }
+					if (!protocol) { return res.status(400).send('no known protocol ' + protid); }
+					/// TODO --- next step is to apply the method to the arguments	
+					var result = fn(config);
+					res.status(200).send(result);
+				});
 			});
-	},
-	deserialise_config:(config) => { 
-		// todo can contain references to particular methods
-		var ops = this.sm.ops;
-		// this is coming in from the saved version of this op.
-		this.config = _(config).keys().reduce((obj,k) => {
-			var serialv = config[k];
-			obj[k] = serialv.type == 'smop' ? ops[serialv.iid] : serialv;
-			return obj;
-		}, {});
-		this.iid = config.iid;
-	},
-	serialise_config:() => {
-		var config = this.config;
-		_(config).keys().reduce((obj,k) => { 
-			var v = config[k];
-			obj[k] = v instanceof SMOp ? { type:'smop', iid: v.iid } : v;
-			return obj;
-		},{});
-		obj.iid = this.iid;
-		return obj;
+		});
 	}
 };
 	
-var SocialMachine = (smvm, instancedoc) => {
+var SocialMachine = (smvm, id) => {
 	this.smvm = smvm;
-	if (instancedoc === undefined) { 
-		// new social machine
-		this.ops = {}; // by iid
-	} else {
-		this.load(instancedoc);
-	}
+	this.ops = {}; // by iid
+	this.id = id;
+	this.db = this.smvm.db.collection(COLLECTION);
+	this.load();
 };
 SocialMachine.prototype = {
 	bind:() => {
@@ -99,18 +89,40 @@ SocialMachine.prototype = {
 			idb = this.smvm.db.collection(COLLECTION);
 
 	},
-	load:function(idoc) {
-		var this_ = this, db = this.smvm.db.collection(COLLECTION), smvm = this.smvm, ops = this.ops || {};
-		this.ops = idoc.ops.map((opspec) => {
-
-			var op = smvm.ops[opspec.iid] || new SMOp(opspec);
-			smvm.ops[opspec.iid] = op;
+	load:function() {
+		// loads all the ops we need
+		var this_ = this, db = this.db, smvm = this.smvm, ops = this.ops || {};
+		// retrieve our doc.
+		return db.findById(this.id).then((idoc) => {
+			this.ops = idoc.ops.map((opspec) => {
+				smvm.ops[opspec.iid] = this_.ops[opspec.iid] = smvm.ops[opspec.iid] || new SMOp(opspec.iid);
+			});
+			return Promise.all(_.values(smvm.ops).map((x) => x.load()));
 		});
-		this.ops = ops;
 	},
-	newOp:(instance, id, config) => {
-		var args = arguments.slice(2),
-			reg = require('./smvm-examples').registry;
+	// creating a new instance
+	newOp:(protid, config_args) => {
+		var iid = uuid.v1(),
+			doc = { _id:iid, type:'smop', protid: protid, config:this.serialise_op_config(config_args) };
+		return this.db.save(doc).then((x) => new SMOp(iid));
+	},
+	serialise_op_config:(config) => {
+		_(config).keys().reduce((obj,k) => { 
+			var v = config[k];
+			obj[k] = v instanceof SMOp ? { type:'smop', iid: v.iid } : v;
+			return obj;
+		},{});
+		return obj;
+	},
+	deserialise_op_callable_config:(config) => { 
+		// todo can contain references to particular methods
+		var ops = this.ops;
+		// this is coming in from the saved version of this op.
+		return _(config).keys().reduce((obj,k) => {
+			var serialv = config[k];
+			obj[k] = serialv.type == 'smop' ? ops[serialv.iid].makeCallable() : serialv;
+			return obj;
+		}, {});
 	}
 };
 
@@ -138,10 +150,7 @@ SMVM.prototype = {
 
 		idb.find({type:'socialmachine'}).then((instances) => {
 			instances.map((i) => { 
-				if (!machines[i.id]) {
-					machines[i.id] = new SocialMachine(this_, i);
-					machines[i.id] = true;
-				}
+				machines[i.id] = machines[i.id] || new SocialMachine(this_, i.id);
 			});
 		});
 	}
